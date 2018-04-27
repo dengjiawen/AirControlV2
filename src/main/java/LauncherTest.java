8d38418b53186394b2fc01803163c7598cdc71ff
Phreaddie <freddengmath@gmail.com>
2018-04-26 22:36:29 -0400

    Revert "plane can now move with correct angle"
    This reverts commit f703774f27a560eeecea3e9cb9b0f76956dd90b1.

--- src/main/java/LauncherTest.java
+++ src/main/java/LauncherTest.java
@@ -1,9 +1,6 @@
 package main.java;
 
 import main.java.common.ThreadUtils;
-import main.java.logic.RefUtils;
-import main.java.music.MusicUtils;
-import main.java.path.MapUtils;
 import main.java.resources.FontResource;
 import main.java.resources.ImageResource;
 import main.java.ui.RenderUtils;
@@ -11,16 +8,12 @@ import main.java.ui.Window;
 
 public class LauncherTest {
 
-    static {
+    public static void main (String... args) {
+
         ImageResource.init();
         FontResource.init();
-        MapUtils.init();
-        ThreadUtils.init();
-        MusicUtils.init();
-        RefUtils.init();
-    }
 
-    public static void main (String... args) {
+        ThreadUtils.init();
 
         Window window = new Window();
 
--- src/main/java/ui/Canvas.java
+++ src/main/java/ui/Canvas.java
@@ -1,17 +1,16 @@
 package main.java.ui;
 
 import main.java.common.ThreadUtils;
-import main.java.logic.Airplane;
-import main.java.logic.RefUtils;
-import main.java.path.CheckPoint;
-import main.java.path.MapUtils;
+import main.java.logic.MapUtils;
 import main.java.resources.ImageResource;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Area;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
 
 public class Canvas extends JPanel {
 
@@ -57,79 +56,37 @@ public class Canvas extends JPanel {
 
         g2d.drawImage(ImageResource.map_YUMA_airport, 0, 0, getWidth(), getHeight(), this);
 
-        paintPath(MapUtils._rwy26, Color.red, g2d);
-        paintPath(MapUtils._rwy35, Color.green, g2d);
-        paintPath(MapUtils._rwy3L, Color.orange, g2d);
-        paintPath(MapUtils._rwy3R, Color.blue, g2d);
+        g2d.setStroke(new BasicStroke(10f));
 
-        paintPath(MapUtils._taxiE, Color.cyan, g2d);
-        paintPath(MapUtils._taxiD, Color.magenta, g2d);
-
-        paintPath(MapUtils._taxiA, Color.darkGray, g2d);
-        paintPath(MapUtils._taxiA1, Color.pink, g2d);
-        paintPath(MapUtils._taxiA2, Color.yellow, g2d);
-        paintPath(MapUtils._taxiA3, Color.white, g2d);
-
-        g2d.setStroke(new BasicStroke(5f));
         g2d.setColor(Color.red);
+        g2d.draw(MapUtils.rwy3L);
 
-        RefUtils.planes.forEach((i, p) -> {
-            paintPlane(p, g2d);
-        });
+        g2d.setColor(Color.green);
+        g2d.draw(MapUtils.rwy3R);
 
-    }
+        g2d.setColor(Color.orange);
+        g2d.draw(MapUtils.rwy26);
 
-    protected void paintPath(ArrayList<CheckPoint> points, Color color, Graphics2D g2d) {
+        g2d.setColor(Color.blue);
+        g2d.draw(MapUtils.rwy35);
 
-        g2d.setColor(color);
-        points.forEach(p -> {
-            g2d.fillOval((int) p.getX(), (int) p.getY(), 10, 10);
-        });
+        g2d.setColor(Color.cyan);
+        g2d.draw(MapUtils.taxiE);
 
-    }
+        g2d.setColor(Color.magenta);
+        g2d.draw(MapUtils.taxiD);
 
-    protected void paintPlane(Airplane plane, Graphics2D g2d) {
+        g2d.setColor(Color.lightGray);
+        g2d.draw(MapUtils.taxiA);
 
-        AffineTransform original = g2d.getTransform();
-        g2d.rotate(Math.PI + plane.current_checkpoint.heading, plane.getX(), plane.getY());
+        g2d.setColor(Color.pink);
+        g2d.draw(MapUtils.taxiA1);
 
-        g2d.drawImage(ImageResource.plane_s,
-                (int)(plane.getX() - 10 - (ImageResource.plane.getWidth() * 0.1)/2),
-                (int)(plane.getY() + 3 - (ImageResource.plane.getHeight() * 0.1)/2),
-                (int)(ImageResource.plane.getWidth() * 0.1), (int)(ImageResource.plane.getHeight() * 0.1), this);
+        g2d.setColor(Color.yellow);
+        g2d.draw(MapUtils.taxiA2);
 
-        g2d.drawImage(ImageResource.plane,
-                (int)(plane.getX() - (ImageResource.plane.getWidth() * 0.1)/2),
-                (int)(plane.getY() - (ImageResource.plane.getHeight() * 0.1)/2),
-                (int)(ImageResource.plane.getWidth() * 0.1), (int)(ImageResource.plane.getHeight() * 0.1), this);
-
-        g2d.drawLine((int)plane.getX(), (int)(plane.getY() - (ImageResource.plane.getHeight() * 0.1)/2),
-                (int)plane.getX(), (int)(plane.getY() + (ImageResource.plane.getHeight() * 0.1)/2));
-
-        g2d.drawOval((int)plane.getX() - 20, (int)(plane.getY() - (ImageResource.plane.getHeight() * 0.1)/2) - 20, 40, 40);
-
-//        if (!plane.strobe_on) {
-//            g2d.drawImage(ImageResource.white_nav,
-//                    (int) (plane.getX() - (ImageResource.plane.getWidth() * 0.1) / 2 - 30),
-//                    (int) (plane.getY()),
-//                    (int) (ImageResource.white_nav.getWidth() * 0.5),
-//                    (int) (ImageResource.white_nav.getWidth() * 0.5), this);
-//            g2d.drawImage(ImageResource.white_nav,
-//                    (int) (plane.getX() + (ImageResource.plane.getWidth() * 0.1) / 2 - 30),
-//                    (int) (plane.getY()),
-//                    (int) (ImageResource.white_nav.getWidth() * 0.5),
-//                    (int) (ImageResource.white_nav.getHeight() * 0.5), this);
-//        }
-//
-//        if (!plane.acl_on) {
-//            g2d.drawImage(ImageResource.red_nav,
-//                    (int) (plane.getX() - (ImageResource.plane.getWidth() * 0.1) / 2 + 15),
-//                    (int) (plane.getY() - 30),
-//                    (int) (ImageResource.red_nav_intense.getWidth() * 0.5),
-//                    (int) (ImageResource.red_nav_intense.getHeight() * 0.5), this);
-//        }
-
-        g2d.setTransform(original);
+        g2d.setColor(Color.white);
+        g2d.draw(MapUtils.taxiA3);
 
     }
 
@@ -157,13 +114,13 @@ public class Canvas extends JPanel {
 
     }
 
-    public static void calcRelMousePoint() {
+    public static void calcRelMousePoint () {
         rel_mouse_point_x = Window.mouse_point_x / zoom_factor - transpose_x;
         rel_mouse_point_y = Window.mouse_point_y / zoom_factor - transpose_y;
 
     }
 
-    public static Point2D calcRelPoint(Point2D point) {
+    public static Point2D calcRelPoint (Point2D point) {
 
         Point2D rel_point = new Point2D.Float();
 
